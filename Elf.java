75ed0b69ad43c51f3cd95f800ef0f8a055fbe964
Rayana77 <124196954+Rayana77@users.noreply.github.com>
2023-02-19 00:19:45 +0300

    Add files via upload

---
+++ Elf.java
@@ -0,0 +1,25 @@
+package game.hero;
+
+import java.util.Scanner;
+
+public class Elf {
+
+    public static void main(String[] args) {
+
+        Scanner scanner  = new Scanner(System.in);
+
+        String s1 = "I study Basic Java!";
+
+        System.out.println("I study Basic Java!");
+
+        System.out.println(s1.charAt(18));
+
+        System.out.println(s1.contains("Java"));
+
+        String s2 = s1.replace(
+
+
+
+
+    }
+}
